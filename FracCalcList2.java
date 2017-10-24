25505e9ca0ad4db034f3b64157cbb6f85f0dbc4b
Toucanic <31905870+Toucanic@users.noreply.github.com>
2017-10-24 09:30:51 -0700

    Add files via upload

--- FracCalcList2.java
+++ FracCalcList2.java
@@ -1,10 +1,32 @@
 package fracCalc;
 
+import java.util.*;
+
 public class FracCalcList2 {
 
+public static final char[] realNums = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	
+	public static Boolean contained(char[] let, char check) {
+		for (char Num : let) {
+			if (check == Num) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner eqRead = new Scanner(System.in);
+		System.out.print("type equation: ");
+		String reck = produceAnswer(equation);
+		System.out.println(reck);
+	}
+	
+	public static String produceAnswer(Scanner eqRead) {
+		String[] elementList = new String[99];
+		for ()
+		String equation = eqRead.next();
+		return null;
 	}
 
 }
